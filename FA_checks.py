83c731a462ad07ccbea7eda6e5ecf2ab4af01cc2
Elise M <elise.morana@efrei.net>
2024-04-19 11:58:11 +0200

    Correction check functions

--- FA_checks.py
+++ FA_checks.py
@@ -1,5 +1,14 @@
 # Functions to check if the given automaton is in a certain form
 
+def is_standard(automaton):
+    if len(automaton["initial_states"]) > 1:
+        return 0
+    for elt in automaton["transitions"]:
+        if elt[2:] in automaton["initial_states"]:
+            return 0
+    return 1
+
+
 def is_deterministic(automaton):
     if len(automaton["initial_states"]) > 1:
         return 0
@@ -11,13 +20,9 @@ def is_deterministic(automaton):
 
 
 def is_complete(automaton):
+    if not is_deterministic(automaton):
+        return 0
     if len(automaton["transitions"]) == (len(automaton["alphabet"])*len(automaton["states"])):
         return 1
-    return 0
-
-
-def is_standard(automaton):
-    for elt in automaton["transitions"]:
-        if elt[2:] in automaton["initial_states"]:
-            return 0
-    return 1
+    else:
+        return 0
