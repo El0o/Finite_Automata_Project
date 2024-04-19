978dbea7be702c81f23d03edb5004ffe2843b923
Elodie <elodie.duflaut@gmail.com>
2024-04-19 12:37:58 +0200

    Fusions des fichiers tools.py et FA_checks.py

--- FA_checks.py
+++ FA_checks.py
@@ -1,6 +1,9 @@
 # Functions to check if the given automaton is in a certain form
 
 def is_standard(automaton):
+    """
+    Returns true if the automaton is standard
+    """
     if len(automaton["initial_states"]) > 1:
         return 0
     for elt in automaton["transitions"]:
@@ -10,6 +13,9 @@ def is_standard(automaton):
 
 
 def is_deterministic(automaton):
+    """
+    Returns true if the automaton is deterministic
+    """
     if len(automaton["initial_states"]) > 1:
         return 0
     for elt in automaton["transitions"]:
@@ -20,9 +26,22 @@ def is_deterministic(automaton):
 
 
 def is_complete(automaton):
+    """
+    Returns true if the automaton is complete
+    """
     if not is_deterministic(automaton):
         return 0
     if len(automaton["transitions"]) == (len(automaton["alphabet"])*len(automaton["states"])):
         return 1
     else:
         return 0
+
+
+def yes_no(boolean):
+    """
+    Translate a boolean into a comprehensible string.
+    """
+    if boolean:
+        return "Yes"
+    else:
+        return "No"
