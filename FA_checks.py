07383b19fc19a4d84da20420d81c34a9616eef69
Enora <122808027+Dalhneor@users.noreply.github.com>
2024-04-20 15:24:07 +0200

    Update avec les print dans deter et comp

--- FA_checks.py
+++ FA_checks.py
@@ -17,10 +17,12 @@ def is_deterministic(automaton):
     Returns true if the automaton is deterministic
     """
     if len(automaton["initial_states"]) > 1:
+        print("The automaton is not deterministic because there is more than 1 initial state.")
         return 0
     for elt in automaton["transitions"]:
         for comp in automaton["transitions"][automaton["transitions"].index(elt):]:
             if (elt[:2] == comp[:2]) and (elt != comp):
+                print("The automaton is not deterministic because there is multiple transition from an initial state.")
                 return 0
     return 1
 
@@ -30,10 +32,12 @@ def is_complete(automaton):
     Returns true if the automaton is complete
     """
     if not is_deterministic(automaton):
+        print("The automaton is not complete because it is not deterministic.")
         return 0
     if len(automaton["transitions"]) >= (len(automaton["alphabet"])*len(automaton["states"])):
         return 1
     else:
+        print("The automaton is not complete because there is not enough transitions.")
         return 0
 
 
