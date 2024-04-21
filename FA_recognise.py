81414b466a895b4f6220cad2f25b438bbf13aa5e
Elodie <elodie.duflaut@gmail.com>
2024-04-21 11:07:13 +0200

    Changements légers pour meilleur implémentation

--- FA_recognise.py
+++ FA_recognise.py
@@ -1,26 +1,25 @@
-from FA_lstest import automata
-
-#reading of a word
+# Reading of a word
 def read_word():
     return input("Type a word: ")
 
-#function that change a word, we remove the first letter of it
-def new_word (word):
-    nword=""
-    for i in range(1,len(word)):
+
+# Function that change a word, we remove the first letter of it
+def new_word(word):
+    nword = ""
+    for i in range(1, len(word)):
         nword += word[i]
     return nword
 
 
-#function that reconise if a word is recognised by a given automaton
-def recognize_word (automaton, word):
+# Function that recognise if a word is recognised by a given automaton
+def recognize_word(automaton, word):
     lenth = len(word)
     lastplace = ""
     initword = word
 
     for i in range(len(automaton['initial_states'])):
         place = automaton['initial_states'][i]
-        #We will test by starting with every initial states.
+        # We will test by starting with every initial states.
         if word == "" and i == len(automaton['initial_states']):
             if lastplace in automaton['final_states']:
                 return True
@@ -32,40 +31,32 @@ def recognize_word (automaton, word):
             else:
                 if len(word) < len(initword):
                     word = initword
-                #This is the comparison algorithm. We check if there is a transition corresponding to our depart state and our letter.
+                # This is the comparison algorithm. We check if there is a transition corresponding to our starting state and our letter.
                 for k in range(lenth):
                     for j in automaton['transitions']:
                         if place + word[0] == j[0] + j[1]:
                             place = j[2]
                             word = new_word(word)
-                            #We store the final state of the word we just check, to verify if it is a final state of the automaton.
+                            # We store the final state of the word we just check, to verify if it is a final state of the automaton.
                             if word == "":
-                                lastplace=place
+                                lastplace = place
                             break
     if automaton != "":
         return False
     else:
         return True
 
-#The main function that will permit to continously ask the user to enter a word until he wants to stop.
-def word_recognition (automaton):
-    word=""
-    print("Write : end if you want to stop testing")
+
+# The main function that will permit to continuously ask the user to enter a word until he wants to stop.
+def word_recognition(automaton):
+    word = ""
+    print("Write [end] to exit word recognition.")
     while word != "end":
         word = read_word()
         if word == "end":
             break
-        test = recognize_word(automaton,word)
-        if test == True:
-            print("The word",word,"is recognised by the automaton n°",automaton['id'])
+        test = recognize_word(automaton, word)
+        if test:
+            print(f"The word {word} is recognised by the automaton n°{automaton['id']}")
         else:
-            print("The word", word, "is not recognised by the automaton n°", automaton['id'])
-    print("The test is finished.")
-
-
-
-print(automata[4])
-word_recognition(automata[4])
-
-
-
+            print(f"The word {word} is not recognised by the automaton n°{automaton['id']}")
