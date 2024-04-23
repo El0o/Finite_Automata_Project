f130658527899793477894ec2af927be8b979cb4
Elodie <elodie.duflaut@gmail.com>
2024-04-23 16:17:42 +0200

    ajout trace_generator.py

---
+++ trace_generator.py
@@ -0,0 +1,22 @@
+from FA_display import *
+from FA_operations import *
+from FA_checks import *
+from FA_files_management import *
+from FA_lst import automata
+
+#automata = import_all_automata()
+
+
+for i in range(44):
+    auto = automata[i]
+    if not is_standard(auto):
+        export_automaton(standardization(auto))
+    if not is_deterministic(auto):
+        export_automaton(determinization(auto))
+    if not is_complete(auto):
+        export_automaton(completion(auto))
+    export_automaton(complementary_automaton(auto))
+
+
+
+
